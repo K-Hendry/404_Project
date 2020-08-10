35e144bc012e37491b4de50a769773dcba05f421
K-Hendry <63383187+K-Hendry@users.noreply.github.com>
2020-08-09 22:52:30 -0400

    fixed mobile spacing section1

--- dev/js/scrollTrigger.js
+++ dev/js/scrollTrigger.js
@@ -2,13 +2,14 @@
     Dog1 Fade
 ============= */
 const DogTimeline = gsap.timeline();
-DogTimeline.from(".dogfaces", {duration:3, alpha:0, stagger:1});
+DogTimeline.from(".dogfaces", {duration:5, alpha:0, stagger:1});
 ScrollTrigger.create({
     animation: DogTimeline,
     toggleActions: "restart none none none",
-    trigger: "dog-img",
-    start: "top 50%",
+    trigger: "error-container",
+    start: "top 10%",
     end: "bottom 50%",
     markers: true,
     id: "dog-img"
 });
+
