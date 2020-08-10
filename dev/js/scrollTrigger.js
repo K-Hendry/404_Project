8e19f41760245c1ef7f3e4f6e154979fcb103262
K-Hendry <63383187+K-Hendry@users.noreply.github.com>
2020-08-10 08:49:30 -0400

    added animation for 404 and page not found

--- dev/js/scrollTrigger.js
+++ dev/js/scrollTrigger.js
@@ -1,16 +1,38 @@
-/* =============
-    Dog1 Fade
-============= */
+/* ========================
+    Dog Animation
+======================== */
 const DogTimeline = gsap.timeline();
-DogTimeline.to(".dogfaces", {duration:3, alpha:0, stagger:2});
+DogTimeline.to(".dogfaces", {duration:7, alpha:0, stagger:4});
 ScrollTrigger.create({
     animation: DogTimeline,
     toggleActions: "restart none none none",
     trigger: "#state-1",
-    start: "bottom 60%",
-    // end: "bottom 50%",
-    markers: true,
+    start: "bottom 90%",
+   // markers: true,
     id: "dog-img",
     scrub: 1
 });
-
+/* ========================
+    404 Animation
+======================== */
+const FourTimeline = gsap.timeline();
+gsap.set(".four",{alpha:0});
+FourTimeline.to(".four", {duration:7, alpha:1, stagger:4});
+ScrollTrigger.create({
+    animation: FourTimeline,
+    toggleActions: "restart none none none",
+    trigger: "#Number1",
+    start: "top 70%",
+    end: "bottom 30%",
+    //markers: true,
+    id: "error-text",
+    scrub: 1
+});
+/* ========================
+    Logo Animation
+======================== */
+gsap.to("#running-text", {
+    duration: 5,
+    xPercent: 200,
+    repeat: -1
+});
