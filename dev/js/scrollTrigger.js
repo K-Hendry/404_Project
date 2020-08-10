dd335f80740d57492a1ba9acdbe5df08ad4b6a34
K-Hendry <63383187+K-Hendry@users.noreply.github.com>
2020-08-09 23:36:44 -0400

    beautified file

--- dev/js/scrollTrigger.js
+++ dev/js/scrollTrigger.js
@@ -2,12 +2,16 @@
     Dog1 Fade
 ============= */
 const DogTimeline = gsap.timeline();
-DogTimeline.from(".dogfaces", {duration:5, alpha:0, stagger:1});
+
+gsap.set(".dogfaces",{alpha:0});
+
+DogTimeline.to(".dogfaces", {duration:5, alpha:1, stagger:1});
+
 ScrollTrigger.create({
     animation: DogTimeline,
     toggleActions: "restart none none none",
-    trigger: "error-container",
-    start: "top 10%",
+    trigger: "scroll-text",
+    start: "top 20%",
     end: "bottom 50%",
     markers: true,
     id: "dog-img"
