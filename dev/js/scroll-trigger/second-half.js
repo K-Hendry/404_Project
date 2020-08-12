f36b34ec7b3c8bceb9ac202bcb1035f5d323e331
K-Hendry <63383187+K-Hendry@users.noreply.github.com>
2020-08-12 05:59:28 -0400

    pt 1 fadeout code

---
+++ dev/js/scroll-trigger/second-half.js
@@ -0,0 +1,16 @@
+/* ========================
+    Pt1 Fadeout Animation
+======================== */
+const Pt1Timeline = gsap.timeline();
+gsap.set(".pt1",{alpha:1});
+Pt1Timeline.to(".pt1", {duration:1, alpha:0});
+ScrollTrigger.create({
+    animation: Pt1Timeline,
+    toggleActions: "restart none none none",
+   trigger: "#error-page2",
+   start: "top 50%",
+   end: "top 50%",
+   markers: true,
+   id: "error-text",
+   scrub: 1
+});
